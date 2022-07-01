c12b58a70339c43b0cb5f9e27e00760848b75764
batuhan <batuhanyemenicigil@gmail.com>
2022-07-01 20:22:01 +0300

    ah

--- lib/ekranlar/loading_screen.dart
+++ lib/ekranlar/loading_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:myapp1/utils/location.dart';
-
+import 'package:http/http.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   State<LoadingScreen> createState() => _LoadingScreenState();
@@ -9,7 +9,26 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
  LocationHelper? locationData;
- 
+ Future<void> getLocationData() async{
+ locationData = LocationHelper();
+ await locationData?.getCurrentLocation();
+ if(locationData!.latitude == null || locationData!.longitude == null){
+print("konum bilgileri gelmiyor.");
+ }
+else {
+  print("latitude: " + locationData!.latitude.toString());
+  print("longitude: "+ locationData!.longitude.toString());
+}
+
+ }
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLocationData();
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
